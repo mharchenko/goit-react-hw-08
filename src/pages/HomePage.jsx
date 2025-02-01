1b4ae4d6d30a9f40d4c023d80877702191059ac5
Mexty <m.harchenko@i.ua>
2025-02-01 17:59:24 +0200

    Added reactMob8_1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,15 @@ const HomePage = () => {
   return (
     <div className="homePage">
       <h1>I am your Phonebook App! </h1>
-      <p>Let's start...</p>
+      <h2>Phonebook App: Your personal assistant for organizing contacts!</h2>
+      <h3>
+        Phonebook App is a convenient and intuitive mobile application that
+        helps you organize all your contacts in one place. Forget about paper
+        notebooks and clutter in the phone book! With the Phonebook App, you can
+        easily add new contacts, edit existing ones, search for the information
+        you need, and much more.
+      </h3>
+      <p>Let's get started, please register...</p>
     </div>
   );
 };
